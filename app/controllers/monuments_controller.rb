9702e5a4e33d7f87f9c9d0c98b9c266637e1e1c1
Maxime Moreira <maxime.moreira@gmail.com>
2023-05-29 16:09:05 +0200

    added monument controller and index show and new views

---
+++ app/controllers/monuments_controller.rb
@@ -0,0 +1,29 @@
+class MonumentsController < ApplicationController
+  before_action :set_monument, only: {:show}
+
+
+  def index
+    @monuments = Monument.all
+  end
+
+  def show
+  end
+
+  def new
+    @monument = Monument.new
+  end
+
+  def create
+    @monument = Monument.new(params_monument)
+  end
+
+  private
+
+  def set_monument
+    @monument = Monument.find[:id]
+  end
+
+  def params_monument
+    params.require(:monument).permit(:name, :picture, :price, :description, :address, :availability)
+  end
+end
