074c3656d7c0aa8eb48e4a923f4f907d7af9d577
Javier Alvarez <javialsal@gmail.com>
2023-05-30 15:39:44 +0200

    correct-show-cloudi

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -5,7 +5,8 @@ class Monument < ApplicationRecord
   has_many_attached :photos
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :photos, presence: true
+  # validates :photos, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :address, presence: true
+
 end
