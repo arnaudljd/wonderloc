82f3d6d0ba71de205fcc1e00aefedf7962014dae
Maxime Moreira <maxime.moreira@gmail.com>
2023-05-31 14:37:28 +0200

    with new features (request and names)

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,10 @@
 class DashboardsController < ApplicationController
 
   def profile
-    @bookings = Booking.where(user_id: (current_user))
-    @monuments = Monument.where(user_id: (current_user))
+    @bookings = current_user.bookings
+    @monuments = current_user.monuments
+    @demands = Booking.all.select {|booking| booking.monument.user == current_user}
+    @future_bookings = @bookings.where(end_date: Date.today) + @bookings.where('start_date >= ?', Date.today)
+    @passed_bookings = @bookings.where('end_date < ?', Date.today)
   end
 end
