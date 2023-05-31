18df379901e69d0a808d742833489fd9e50acea4
Javier Alvarez <javialsal@gmail.com>
2023-05-31 12:04:18 +0200

    new photo to users on new registrations with cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :monuments, dependent: :destroy
   has_many :reviews, dependent: :destroy
-
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
