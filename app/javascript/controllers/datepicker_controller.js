31fd1f1209b36a2fe3a24f61807121a838c3d147
Youval Akouka <youvalakouka@gmail.com>
2023-06-02 12:57:48 +0200

    Edit booking dates validation fix v2b

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
-// Connects to data-controller="datepicker"
+// Connectss to data-controller="datepicker"
 export default class extends Controller {
 
   static targets = ['start', 'end', 'sub', 'price']
